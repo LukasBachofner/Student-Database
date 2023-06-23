c0cbb3d2f385da308f2142fa5b825077fdeb3d5c
Lukas Bachofner <l.m.bachofner@gmail.com>
2023-06-23 22:51:05 +0200

    feat: (sha256) password is hashed during network transport

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -12,11 +12,11 @@ import fetch from 'node-fetch';
 export class LoginComponent {
   
   async Backend_API_Login() {
-    
+    var SHA256 = require("crypto-js/sha256");
   
     const username = (<HTMLInputElement>document.getElementById('username')).value;
-    console.log(username);
-    const password = (<HTMLInputElement>document.getElementById('password')).value;
+    const password = String(SHA256((<HTMLInputElement>document.getElementById('password')).value));
+    console.log(password);
     const json_body = {"username":username, "password":password}
     const message = await fetch("http://localhost:5000/login",  {method:"POST", body: JSON.stringify(json_body), headers: {'Content-Type': 'application/json'}});
   
